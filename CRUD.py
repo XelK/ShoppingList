f7d81fb0f180cf7960ee80fa76af8a8b5f57c489
xelk <haveiteasy@gmail.com>
2021-11-23 11:45:27 +0100

    Update CRUD implementation

--- CRUD.py
+++ CRUD.py
@@ -24,22 +24,40 @@ class Crud:
             return self.resp("OK", f"Object {product['name']} INSERTED")
         return result
 
-    def retrieve(self, product):
-        result = self.db.products.find_one(product)
+    def extract_params(self, params):
+        name = params.get("name")
+        quantity = params.get("quantity")
+        description = params.get("description")
+        query = {}
+        if name:
+            query['name'] = name
+        if quantity:
+            query['quantity'] = int(quantity)
+        if description:
+            query['description'] = description
+        return query
+
+    def retrieve(self, product=None):
+        query = self.extract_params(product)
+        result = self.db.products.find(query, {"_id": False})
         if result:
-            return self.resp("OK", f'"name":{result["name"]},'
-                                   f'"quantity":{result["quantity"]},'
-                                   f'"description":{result["description"]}')
+            return self.resp("OK", list(result))
         return self.resp("ERROR", f"Object {product['name']} NOT FOUND")
 
-    def update(self, product):
-        result = self.db.products.update_one({"name": product['name']}, {"$set": product})
+    #    def update(self, product):
+    #        result = self.db.products.update_one({"name": product['name']}, {"$set": product})
+    #        if result.modified_count:
+    #            return self.resp("OK", f"Object {product['name']} UPDATED")
+    #        return self.resp("ERROR", f"Object {product['name']} NOT FOUND")
+
+    def update(self, product_from, product_to):
+        result = self.db.products.update_one({"name": product_from['name']}, {"$set": product_to})
         if result.modified_count:
-            return self.resp("OK", f"Object {product['name']} UPDATED")
-        return self.resp("ERROR", f"Object {product['name']} NOT FOUND")
+            return self.resp("OK", f"Object {product_from['name']} UPDATED")
+        return self.resp("ERROR", f"Object {product_from['name']} NOT FOUND")
 
     def delete(self, product):
-        result = self.db.products.find_one_and_delete({"name": product['name']})
+        result = self.db.products.find_one_and_delete(product)
         if result:
             return self.resp("OK", f"Object {product['name']} DELETED")
         return self.resp("ERROR", f"Object {product['name']} NOT FOUND")
